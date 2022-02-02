1efcd2aeb7211241d38134a236edf7268b8beb8a
carlo catayna <carlocatayna13@gmail.com>
2022-02-02 21:55:22 +0800

    finished the game

--- script.js
+++ script.js
@@ -20,34 +20,43 @@ function playGame(player,computer){
     //computer wins scenario
     if(player=="rock" && computer=="paper"){
         console.log("Computer wins, "+ computer + " beats " + player);
+        return 1;
     }
     else if(player=="paper" && computer=="scissors"){
         console.log("Computer wins, "+ computer + " beats " + player);   
+        return 1;
     }
     else if(player=="scissors" && computer=="rock"){
         console.log("Computer wins, "+ computer + " beats " + player);   
+        return 1;
     }
 
     //player wins scenario
     else if(player=="rock" && computer=="scissors"){
         console.log("Player wins, "+ player + " beats " + computer);   
+        return 2;
     }
     else if(player=="paper" && computer=="rock"){
         console.log("Player wins, "+ player + " beats " + computer);   
+        return 2;
     }
     else if(player=="scissors" && computer=="paper"){
         console.log("Player wins, "+ player + " beats " + computer);   
+        return 2;
     }
 
     //nobody wins
     else if(player=="rock" && computer=="rock"){
         console.log("Nobody wins. " + computer + " and " + player);    
+        return 3;
     }
     else if(player=="paper" && computer=="paper"){
         console.log("Nobody wins. " + computer + " and " + player);        
+        return 3;
     }
     else if(player=="scissors" && computer=="scissors"){
         console.log("Nobody wins. " + computer + " and " + player);        
+        return 3;
     }
     
     //error
@@ -56,7 +65,26 @@ function playGame(player,computer){
     }
 }
 
-let playerdecision = String(prompt("Enter your decision:"));
+function game(){
+    let i;
+    playerScore = 0;
+    computerScore = 0;
+    for(i=0;i<5;i++){
+        let playerdecision = String(prompt("Enter your decision:"));
+        let winner = playGame(playerdecision,computerPlay());
+        if(winner == 1){
+            computerScore++;
+        }
+        else if(winner == 2){
+            playerScore++;
+        }
+    }
+    if(playerScore>computerScore){
+        console.log("Player wins!!!");
+    }
+    else{
+        console.log("Computer wins!!");
+    }
+}
 
-
-playGame(playerdecision,computerPlay())
+game();
